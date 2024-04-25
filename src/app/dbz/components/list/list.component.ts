ba34932ed8594d5162b92ffbe1f15a39683600e9
Juan-CT <ct.juan.88@gmail.com>
2024-04-25 18:40:39 +0200

    ngForms y eventEmitters

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -14,4 +14,11 @@ export class ListComponent {
     name: 'Trunks',
     power: 10
   }]
+
+  @Output()
+  onDeleteId: EventEmitter<number> = new EventEmitter();
+
+  onDeletecharacter(index: number): void {
+    this.onDeleteId.emit(index);
+  }
  }
